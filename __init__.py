9928fca32848f456e57a59e9d2b237fa3831dd22
Ben Duffield <ben@wintermute.domain.org>
2008-10-24 12:15:05 +0100

    added cover picture to book_detail, changed style of header (added bg image)

--- __init__.py
+++ __init__.py
@@ -1,18 +1,28 @@
 '''
 TODO LIST
 DONE - make other info for user profile accept null/blank
+
+ - only allow people who made stuff able to edit stuff!!!
+- put links for editing if user created the content
 - make frontpage sane
 DONE - enable display of reviews, etc
 DONE - do add/review/?forbook = some id
 - register page
 - edit stuff - WTF DOES THAT MEAN - write some decent TODOS nublar
 DOEN in DATABASE- book pictures?
-- add pictures to book_detail
+DONE - add pictures to book_detail
 - MOAR/tidy css
 - isbn auto fill
 - let ISBN be optional
 DONE - add voting for books
 DONE - in users.views use get or create for profile
 - paginate book display
-- make pictures have to be approved by someone with permissions
+DONE- make pictures have to be approved by someone with permissions
+ - add in capablitiy to approve book picture 
+- maybe redirect to showing book after creation
+- order form fields
+- make valid html
+- add isbn to book detail
+- add favicon(is that correct, no) add little image to thingy
+- 
 '''
