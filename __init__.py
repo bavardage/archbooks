e1abece4dc34ffcb46144dac847fde3f1d2e4b9f
Ben Duffield <ben@wintermute.domain.org>
2008-12-10 21:34:43 +0000

    todo file updated

--- __init__.py
+++ __init__.py
@@ -1,5 +1,30 @@
 '''
 TODO LIST
+Books are a collection of ISBNs
+ - what is cached locally, and what happens when local clashes with ISBN?
+ - what happens when another ISBN is added - is this just added ad-hoc, is it checked to make sure title and author match the other ISBN(s), what if there are subtle spelling differences, or e.g. The Golden Compass vs Northern Lights - probably better to make it creator-controlled
+ - who has ultimate control over books - the creator, the users, mods?
+ - things like cover images, maybe should be multiple submissions, the top voted one is shown with something like 'view others' below it
+ - should links for books be creator or users?
+ - maybe like the add a review mechanism, an add-a-link?
+ - should there be comments too? comments being less than a review
+ - should reviews be approved first?
+
+Series
+ - series are a collection of books
+
+
+
+
+
+
+
+
+
+------------------
+MOSTLY OBSELETE
+---------------
+
 - frontpage
 - navigation
 - set pagination to be sane, make it configable
@@ -45,7 +70,7 @@ DONE - add favicon(is that correct, no) add little image to thingy
 - some way of editing author names etc if incorrect, or make people confirm beforehand, or something
  - also figure out a way of stopping books written by Ass Face or w/e - maybe again use isbn for this
 - make a genre hierarchy
-- update GIT say last.fm for books
+IDONE - update GIT say last.fm for books
 DONE - replace HTTP_ROOT with {{site}}
 DONE - make login form have registration form built in - fancy javascript showy hidey etc
 - make activated have login form already autofilled
